6c669897422ceea68202e84a1e97d7278558c2f9
Bilal Tahir <btahir@ultimus.com>
2024-02-06 19:52:01 +0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet ]
 })
 export class AppComponent {
   title = 'angular-course';
+
+
+
+  
 }
