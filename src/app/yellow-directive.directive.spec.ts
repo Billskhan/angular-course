fd953852f68317d7130b32aaf43f363822c46e37
Bilal Tahir <btahir@ultimus.com>
2024-02-19 18:02:18 +0500

    Remove

--- src/app/yellow-directive.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { YellowDirectiveDirective } from './yellow-directive.directive';
-
-describe('YellowDirectiveDirective', () => {
-  it('should create an instance', () => {
-    //const directive = new YellowDirectiveDirective();
-    //expect(directive).toBeTruthy();
-  });
-});
