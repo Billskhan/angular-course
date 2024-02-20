fc65447141b1fc3e383e62f7fc2f6a328d5cab79
Bilal Tahir <btahir@ultimus.com>
2024-02-21 03:32:40 +0500

    commit1

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,15 +1,17 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Course } from '../model/course';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'course-card',
   standalone: true,
-  imports: [],
+  imports: [NgIf,NgClass],
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.css'
 })
 export class CourseCardComponent {
 @Input({required:true}) course!:Course;
+@Input() cardIndex!: number;
 @Output() buttonevent = new EventEmitter<Course>();
 
   constructor(){
@@ -19,5 +21,18 @@ export class CourseCardComponent {
   buttonClick(){
     this.buttonevent.emit(this.course)
   }
-}
+  isImageVisable(){
+    return this.course.iconUrl
+  }
 
+  cardClasses():any{
+    if(this.course.category == 'BEGINNER'){
+      return 'beginner';
+    }
+    // return {
+    //   'beginner': true,
+    //   'intermediate': this.course.category=='INTERMEDIATE',
+    // }
+  }
+
+}
